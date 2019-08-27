dd2afdcc3890d5f5560ceb73ed00c6e7e139e81e
Sebas <seplarui@gmail.com>
2019-08-27 19:42:53 +0200

    CORS

--- angular-node-express-api/app.js
+++ angular-node-express-api/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var generate_uid = require('./routes/generate_uid');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1/generate_uid', generate_uid);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -36,6 +39,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 module.exports = app;
