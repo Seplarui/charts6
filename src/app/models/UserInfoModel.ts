2cac44391cea945edc334d8562003d58775a1afa
Sebas <seplarui@gmail.com>
2019-08-27 19:04:43 +0200

    Created Model

---
+++ src/app/models/UserInfoModel.ts
@@ -0,0 +1,18 @@
+export class UserInfoModel {
+    guid: String;
+    customerUid: string;
+
+    first_name: string;
+    last_name: string;
+
+    email: string;
+    zipcode: string;
+
+    password: string;
+
+    constructor(obj: any = null) {
+        if (obj != null) {
+            Object.assign(this, obj);
+        }
+    }
+}
